7f01486ce656f5939b93b83cac19cf974c3e57a6
timur <t.worck.m@gemail.com>
2023-05-31 10:29:41 +0200

    hw8+++

--- FirstMonth/src/Homework/HW8.java
+++ FirstMonth/src/Homework/HW8.java
@@ -74,7 +74,9 @@ public class HW8 {
 
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+            if(arr[i] % 2 != 0){
+                sum += arr[i];
+            }
         }
         return sum;
     }
