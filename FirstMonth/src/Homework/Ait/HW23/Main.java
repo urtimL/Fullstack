989dc4c1263ef7aae3e73ebf6e09edb9851d5ca7
timur <t.worck.m@gemail.com>
2023-06-20 22:47:29 +0200

    hw23

---
+++ FirstMonth/src/Homework/Ait/HW23/Main.java
@@ -0,0 +1,50 @@
+package Homework.Ait.HW23;
+
+public class Main {
+    public static void main(String[] args) {
+        int length = args.length;
+        if(length == 0) {
+            printResult("empty");
+        }
+
+        for (int i = 0; i < length; i++) {
+            printResult(args[i]);
+        }
+    }
+
+    private static void printResult(String str) {
+        switch (str) {
+            case "empty" :
+            case "byte" :
+                System.out.println("byte max value: " + Byte.MAX_VALUE);
+                System.out.println("byte min value: " + Byte.MIN_VALUE);
+                if (str.equals("byte")) { break; }
+            case "int" :
+                System.out.println("int max value: " + Integer.MAX_VALUE);
+                System.out.println("int min value: " + Integer.MIN_VALUE);
+                if (str.equals("int")) { break; }
+            case "short" :
+                System.out.println("short max value: " + Short.MAX_VALUE);
+                System.out.println("short min value: " + Short.MIN_VALUE);
+                if (str.equals("short")) { break; }
+            case "long" :
+                System.out.println("long max value: " + Long.MAX_VALUE);
+                System.out.println("long min value: " + Long.MIN_VALUE);
+                if (str.equals("long")) { break; }
+            case "char" :
+                System.out.println("char max value: " + Character.MAX_VALUE);
+                System.out.println("char min value: " + Character.MIN_VALUE);
+                if (str.equals("char")) { break; }
+            case "float" :
+                System.out.println("float max value: " + Float.MAX_VALUE);
+                System.out.println("float min value: " + Float.MIN_VALUE);
+                if (str.equals("float")) { break; }
+            case "double" :
+                System.out.println("double max value: " + Double.MAX_VALUE);
+                System.out.println("double min value: " + Double.MIN_VALUE);
+                break;
+            default:
+                System.out.println("Wrong type - " + str);
+        }
+    }
+}
